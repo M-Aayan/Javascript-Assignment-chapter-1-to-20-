218256bd24e9542989bcc474b41a89559401f1f1
Muhammad Aayan Shaikh <184842839+M-Aayan@users.noreply.github.com>
2025-05-30 23:20:08 +0500

    js ch:1 done

---
+++ ch1(alert)/main.js
@@ -0,0 +1,8 @@
+alert("Welcome to my website")
+prompt("Enter Your Password")
+alert("Error! Please enter a valid password.")
+alert("Welcome to JS land...\nHappy Coding!")
+alert("Welcome to JS land...")
+alert("Happy Coding!\nPrevent this page from making any additional dialouges")
+alert("Hello... I can run JS through my web browser's console")
+
