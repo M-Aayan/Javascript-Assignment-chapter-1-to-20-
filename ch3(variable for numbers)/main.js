091a112db2a18a11fd1dfde68f09302cdd07ab53
Muhammad Aayan Shaikh <184842839+M-Aayan@users.noreply.github.com>
2025-06-03 22:51:24 +0500

    ch3 done

---
+++ ch3(variable for numbers)/main.js
@@ -0,0 +1,18 @@
+var age = ("I am 19 years old")
+alert(age)
+
+// samaj nhi arha i'll do later 
+//  Declare & initialize a variable to keep track of how many 
+// times a visitor has visited a web page. Show his/her 
+// number of visits on your web page. For example: “You 
+// have visited this site N times”.
+
+var BirthYear = ("my birth year is Nov 2005" + "<br/>")
+document.write(BirthYear)
+
+
+var customerName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+var message = customerName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.";
+document.write(message);
