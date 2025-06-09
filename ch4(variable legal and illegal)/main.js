083026a51bc669afb13c2930b35ef98cd602ef8a
Muhammad Aayan Shaikh <184842839+M-Aayan@users.noreply.github.com>
2025-06-09 17:04:07 +0500

    ch4 done

---
+++ ch4(variable legal and illegal)/main.js
@@ -0,0 +1,12 @@
+var a = 10, b = 20, c = 30;
+
+// legal variable 
+var myName, _user, $price, user1, camelCase;
+
+
+// Illegal variable names (These will cause errors if uncommented) i got from chtgpt
+    // var 1name;       // Starts with a number
+    // var @value;      // Uses invalid character (@)
+    // var user-name;   // Hyphens not allowed
+    // var var;         // JS keyword
+    // var alert;       // Reserved function name
